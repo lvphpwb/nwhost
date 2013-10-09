bd59f59e02781e49b8c2e0f9bf8836fe10a1febe
wangbo6 <wangbo6@360.cn>
2013-10-09 09:59:18 +0800

    修改写注册表方式

--- app/js/conf.js
+++ app/js/conf.js
@@ -2,3 +2,5 @@
 var systempath = 'c';
 //note 程序启动打开地址(默认为空白页)
 var initurl = "about:blank";
+//note 代理端口
+var proxyport = '8080';
--- proxy.bat
+++
@@ -1,3 +0,0 @@
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "http=127.0.0.1:8080" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "" /f
